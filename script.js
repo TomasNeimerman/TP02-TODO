3fd1bd07657fbe1d895834d7bb72d9eb64a39164
neotictom <128648747+neotictom@users.noreply.github.com>
2024-03-31 17:01:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let todos = [];
+
+function AgregarTodo() {
+    const todoInput = document.getElementById("todoInput");
+    const todoText = todoInput.value.trim();
+    
+    if (todoText !== "") {
+        const todo = {
+            text: todoText,
+            completed: false,
+            timestamp: new Date()
+        };
+        todos.push(todo);
+        todoInput.value = "";
+        mostrarTodos();
+    }
+}
+
+function MostrarCompletos(index) {
+    todos[index].completed = !todos[index].completed;
+    mostrarTodos();
+}
+
+function Borrar(index) {
+    todos.splice(index, 1);
+    mostrarTodos();
+}
+
+function mostrarTodos() {
+    const todoList = document.getElementById("todoList");
+    todoList.innerHTML = "";
+    
+    todos.forEach((todo, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = todo.text;
+        if (todo.completed) {
+            listItem.classList.add("Completo");
+            listItem.innerHTML += ` (Hecho a las ${todo.timestamp.toLocaleString()})`;
+        } else {
+            const completeButton = document.createElement("button");
+            completeButton.textContent = "Completar";
+            completeButton.onclick = () => MostrarCompletos(index);
+            listItem.appendChild(completeButton);
+        }
+        
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Borrar";
+        deleteButton.onclick = () => Borrar(index);
+        listItem.appendChild(deleteButton);
+        
+        todoList.appendChild(listItem);
+    });
+}
+
+function TareaMasRapida() {
+    const fastestTask = todos.reduce((prev, curr) => {
+        return prev.timestamp < curr.timestamp ? prev : curr;
+    });
+    alert(`TODO mas rapido: ${fastestTask.text} (Hecho a las ${fastestTask.timestamp.toLocaleString()})`);
+}
